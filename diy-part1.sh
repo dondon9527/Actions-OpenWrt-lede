4d24ba619b96cea25b098cced5f3f9fd6aa5bcf7
麥克奈特 <zhoubin04@gmail.com>
2021-06-23 00:48:33 +0800

    helloworld

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,7 +10,7 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
+ Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
